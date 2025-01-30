ae74cf166d6da97683847f4bd216728a7b2b040c
jborges <Joris.bgs@outlook.fr>
2025-01-30 15:29:39 +0100

    animation correction in transcription page

--- app/components/description/resume.tsx
+++ app/components/description/resume.tsx
@@ -5,30 +5,45 @@ import { View, Text, StyleSheet, TouchableOpacity, Dimensions, Animated } from "
 export default function Resume({ item }: { item: string }) {
     const [itemState, setItemState] = useState(0);
     const heightAnim = useRef(new Animated.Value(40)).current; // Valeur animée pour la hauteur
+    const minWidthAnim = useRef(new Animated.Value(0)).current; // Valeur animée pour la largeur
 
     useEffect(() => {
-        let toValue;
+        let toHeightValue;
         if (itemState === 0) {
-            toValue = 40;
+            toHeightValue = 40;
         } else if (itemState === 1) {
-            toValue = 100;
+            toHeightValue = 100;
         } else {
             // Calculez la hauteur relative en pixels
             const screenHeight = Dimensions.get("window").height;
-            toValue = screenHeight * 0.80;
+            toHeightValue = screenHeight * 0.80;
+        }
+
+        let toWidthValue;
+        const screenWidth = Dimensions.get("window").width;
+        if (itemState === 0) {
+            toWidthValue = screenWidth * 0.80;
+        } else {
+            toWidthValue = screenWidth * 0.90;
         }
 
         Animated.timing(heightAnim, {
-            toValue,
-            duration: 300, // Durée de l'animation en millisecondes
+            toValue: toHeightValue,
+            duration: 200, // Durée de l'animation en millisecondes
             useNativeDriver: false, // Désactiver l'utilisation du pilote natif pour les animations de mise en page
         }).start();
-    }, [itemState, heightAnim]);
+
+        Animated.timing(minWidthAnim, {
+            toValue: toWidthValue,
+            duration: 200, // Durée de l'animation en millisecondes
+            useNativeDriver: false, // Désactiver l'utilisation du pilote natif pour les animations de mise en page
+        }).start();
+    }, [itemState, heightAnim, minWidthAnim]);
 
     const styles = StyleSheet.create({
         container: {
             display: 'flex',
-            justifyContent: itemState == 0 ? 'center' : 'flex-start',
+            justifyContent: 'flex-start',
 
             padding: 5,
             minWidth: itemState != 0 ? '90%' : '80%',
@@ -55,7 +70,7 @@ export default function Resume({ item }: { item: string }) {
     };
 
     return (
-        <Animated.View style={[styles.container, { height: heightAnim }]}>
+        <Animated.View style={[styles.container, { height: heightAnim, minWidth: minWidthAnim }]}>
             <TouchableOpacity onPress={handlePress}>
                 {/* <View style={styles.container}> */}
                     <Text style={styles.text}>RESUME 1</Text>
